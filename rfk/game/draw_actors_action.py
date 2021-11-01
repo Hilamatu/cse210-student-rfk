8043af5e935a2fa30f8b53caaa4882744d61555c
Hilamatu <hil21006@byui.edu>
2021-11-02 05:46:12 +0900

    finished

--- rfk/game/draw_actors_action.py
+++ rfk/game/draw_actors_action.py
@@ -1,3 +1,29 @@
 from game.action import Action
+from game import actor
 
-# TODO: Define the DrawActorsAction class here
+class DrawActorsAction(Action):
+    """A code template for Draw Actors Action. 
+    The responsibility of this class of objects is to draw the the actor action based on the input.
+    
+    Stereotype:
+        Information Holder, Controller
+    """
+
+    def __init__(self, output_service):
+
+        self._output_service = output_service
+
+
+    def execute(self, cast):
+
+        # Clear the screen
+        self._output_service.clear_screen()
+
+        # Draw actor
+        for group in cast.values():
+            self._output_service.draw_actors(group)
+
+        # Flush the Buffer
+        self._output_service.flush_buffer()
+
+    
